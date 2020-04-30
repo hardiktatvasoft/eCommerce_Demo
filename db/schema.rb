383e7f27f784d4e5ebdf8d7dba87699c73e7607c
manharzala <manharsinh.zala@tatvasoft.com>
2020-04-30 10:42:05 +0530

    add quantity field to products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200424122750) do
+ActiveRecord::Schema.define(version: 20200430050459) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -66,6 +66,16 @@ ActiveRecord::Schema.define(version: 20200424122750) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string "data_file_name", null: false
+    t.string "data_content_type"
+    t.integer "data_file_size"
+    t.string "type", limit: 30
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type"
+  end
+
   create_table "cms_pages", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -103,6 +113,7 @@ ActiveRecord::Schema.define(version: 20200424122750) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.integer "quantity"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
